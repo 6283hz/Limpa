5f61ff7fe28b9318dc1e27de56f09d55088cc2ab
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-25 15:26:36 +0900

    initialize project

---
+++ main.js
@@ -0,0 +1,34 @@
+const { Client, Events, EmbedBuilder, GatewayIntentBits } = require('discord.js');
+const { token, client_id, guild_id } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildMessages]});
+
+const exampleEmbed = new EmbedBuilder()
+    .setColor(0x0099FF)
+    .setTitle('Qilte')
+    .setURL('https://discord.gg/36tCw3kYuJ')
+    .setAuthor({ name: 'Limpa', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on('messageCreate', msg => {
+    if(!msg.author.bot){
+        msg.reply({embeds: [exampleEmbed]})
+    }
+})
+
+client.on('interactionCreate', interaction => {
+    if(!interaction.isChatInputCommand) return
+    if(interaction.commandName === 'discord'){
+        interaction.reply({embeds: [exampleEmbed]})
+    }
+})
+
+// Log in to Discord with your client's token
+client.login(token);
---
+++ slash.js
@@ -0,0 +1,28 @@
+const { REST, Routes } = require('discord.js')
+const { token, cliend_id, guild_id } = require('./config.json')
+
+const commands = [
+    {
+        name: 'discord',
+        description: 'hey',
+    },
+];
+
+const rest = new REST({version: '10'}).setToken(token);
+
+(async() => {
+try {
+    console.log("Register commands...")
+
+    await rest.put(
+        Routes.applicationGuildCommands("1164519791189573714", "1142771139010117743"),
+        {
+            body: commands
+        }
+    )
+
+    console.log("Registered succesfully")
+} catch(error){
+    console.error(`There was an error ${error}`)
+}
+})();
